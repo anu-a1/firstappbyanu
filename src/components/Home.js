80b10bcdf85e8cb771d6d017ff4af34d91d03a87
anu-a1 <anupamaachuri@gmail.com>
2017-06-08 11:32:12 -0400

    FirstApp
    first

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>in new com</p>
+                {2+2}
+            </div>
+        )
+    }
+}
+
+Home.pro
